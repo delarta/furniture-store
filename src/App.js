bafc85895b2eab92e879429f4a79fd0aeb59f2af
Delarta Tok Adin <delarta.tok@gmail.com>
2020-06-02 23:05:51 +0700

    Adding simple animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./assets/style/style.scss";
+
+import { useIntersection } from "react-use";
+import {gsap} from "gsap"
 
 import bannerImg from "./assets/img/sofa-banner.png";
 import chairImg1 from "./assets/img/chair-5.png";
@@ -7,6 +10,33 @@ import chairImg2 from "./assets/img/chair-4.png";
 import chairImg3 from "./assets/img/chair-3.png";
 
 function App() {
+
+  const sectionRef = useRef(null);
+
+  const intersectionCatalog = useIntersection(sectionRef, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.8
+  })
+
+  const fadeIn = element => {
+    console.log("fade in")
+    gsap.to(element, 1, {
+      opacity: 1,
+      y: -60,
+      ease: "power4.out",
+      stagger: {
+        amount: 0.5
+      }
+    })
+  }
+
+  useEffect(()=> {
+    fadeIn(".hero-el")
+  }, [])
+
+  intersectionCatalog && intersectionCatalog.intersectionRatio > 0.8 && fadeIn(".fadeIn")
+
   return (
     <div className="App">
       <nav>
@@ -20,15 +50,14 @@ function App() {
       </nav>
       <div id="hero">
         <div className="hero-left">
-          <div>
+          <div className="hero-el">
             <h1>A Nice Furniture.</h1>
             <h1>A Happy Future.</h1>
           </div>
-          <div>
+          <div className="hero-el">
             <p>
-              Find yourself a nice furniture <br />
-              in our rich collection of stuff. <br />
-              Shopping for high grade <br />
+              Find yourself a nice furniture in our rich <br />
+              collection of stuff. Shopping for high grade<br />
               furniture has never been this easy. <br />
             </p>
           </div>
@@ -36,17 +65,17 @@ function App() {
             <button>Shop Now</button>
           </div>
         </div>
-        <div className="hero-right">
+        <div className="hero-el hero-right">
           <img src={bannerImg} alt="sofa-banner" />
         </div>
       </div>
 
-      <div id="catalog">
+      <div id="catalog" ref={sectionRef}>
         <div className="white-box"></div>
         <div className="catalog-content">
           <h2>New Arrival</h2>
           <div className="product-list">
-            <div className="product">
+            <div className="fadeIn product">
               <div className="product-image">
                 <img src={chairImg1} alt="curvy-chair" />
               </div>
@@ -58,7 +87,7 @@ function App() {
                 <div className="product-price">$200</div>
               </div>
             </div>
-            <div className="product">
+            <div className="fadeIn product">
               <div className="product-image">
                 <img src={chairImg2} alt="wooden-chair" />
               </div>
@@ -70,7 +99,7 @@ function App() {
                 <div className="product-price">$200</div>
               </div>
             </div>
-            <div className="product">
+            <div className="fadeIn product">
               <div className="product-image">
                 <img src={chairImg3} alt="comfy-chair" />
               </div>
